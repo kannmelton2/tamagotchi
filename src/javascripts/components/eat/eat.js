d472b7d1c4fdff6fb09353149686ce970fdfe9a9
Kayla Melton <kannmelton2@gmail.com>
2020-03-08 19:09:09 -0500

    wip event listener working

--- src/javascripts/components/eat/eat.js
+++ src/javascripts/components/eat/eat.js
@@ -2,15 +2,28 @@ import petData from '../../helpers/data/petData';
 import utils from '../../helpers/utils';
 import './eat.scss';
 
+const fullnessMeter = (event) => {
+  let full = petData.getFullVal();
+  if (event.currentTarget.id === 'healthy') {
+    full += 10;
+    console.error('healthy', full);
+  } else {
+    full -= 2;
+    console.error('unhealthy', full);
+  }
+};
+
 const foodTracker = () => {
   const domString = `<div>
   <h2>Achilles</h2>
   <p>Feed healthy food</p>
-  <button><i class="fas fa-apple-alt"></i></button>
+  <button class="feedPet" id="healthy"><i class="fas fa-apple-alt"></i></button>
   <p>Feed unhealthy food</p>
-  <button><i class="fas fa-cookie-bite"></i></button>
+  <button class="feedPet" id="unhealthy"><i class="fas fa-cookie-bite"></i></button>
   <h3>${petData.getFullVal()}</h3>
   </div>`;
   utils.printToDom('eat', domString);
+  $('.feedPet').on('click', fullnessMeter);
 };
+
 export default { foodTracker };
